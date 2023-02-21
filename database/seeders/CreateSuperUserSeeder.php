035189bd776e3b1ff47e30f9a1aa8acf76b606e1
Daniellizh <dl.lucky.36@gmail.com>
2023-02-21 18:40:20 +0300

    create superUser(admin) seeder

---
+++ database/seeders/CreateSuperUserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+use Spatie\Permission\Models\Role;
+
+class CreateSuperUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superUser = User::create([
+            'name'=>'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('12341234'),
+            'created_at'=>Carbon::now(),
+            'updated_at'=>Carbon::now()
+        ]);
+
+        Role::create([
+            'name'=>'super-user',
+            'created_at'=>Carbon::now(),
+            'updated_at'=>Carbon::now()
+        ]);
+
+        $superUser->assignRole('super-user');
+    }
+}
